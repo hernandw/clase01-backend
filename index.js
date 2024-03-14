1d551004ecbdfdfb29b13ec11211c55548a9a1e1
Williams Hernandez <hernandw@gmail.com>
2024-03-14 15:04:08 -0300

    rutas

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-console.log('Hola mundo en express')
-console.log(2 + 2)
-console.log(5 * 8)
-console.log('hola')
+const express = require('express');
+
+
+const app = express();
+const port = 3001 
+
+app.get('/', (req, res) => res.send('Hello World desde express!'));
+
+app.get('/about', (req, res) => {
+    
+    res.send('About page!, ' + name + ' ' + suma);
+})
+
+app.get('/contact', (req, res) => res.json({ name: 'John', age: 30 }));
+
+app.get('/product', (req, res) => res.send('Product page!'));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
